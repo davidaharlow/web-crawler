b6ca5c3f7bb51e1b50b14a0949ce8c7b3e8851c1
Bryce Sorensen <26312790+brycessorensen@users.noreply.github.com>
2018-03-21 17:31:18 -0700

    import Search component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import Search from './Components/Search/Search';
 
 class App extends Component {
   render() {
@@ -8,11 +9,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Web Crawler</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-search">
+          <Search />
+        </div>
       </div>
     );
   }
