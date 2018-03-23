afc64a76ad35c16a4cadb7bf76778a4ee65cd59d
Bryce Sorensen <26312790+brycessorensen@users.noreply.github.com>
2018-03-23 00:12:11 -0700

    add functional stream

--- puppeteer.js
+++ puppeteer.js
@@ -1,15 +1,13 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-const scrape = (stream, elements) => {
-  console.log(elements);
-  elements.forEach((element) => {
-    let movie = {
-      title: element.childNodes[1].innerText,
-      description: element.childNodes[7].innerText,
-      director: element.childNodes[9].children[0].text,
-    };
-    stream.write('hey', 'utf-8', err => console.log(err));
+const scrape = (stream, movies) => {
+  movies.forEach((movie) => {
+    stream.write(`${movie}\n`, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   });
 };
 
@@ -17,29 +15,39 @@ const navigate = async (url = 'https://www.imdb.com/search/title?groups=top_1000
   const stream = fs.createWriteStream('movies.txt', 'utf-8', { flags: 'a' });
   const browser = await puppeteer.launch({
     headless: false,
-    slowMo: 5,
   });
   const page = await browser.newPage();
   await page.goto(url);
 
   while (page.$('.lister-page-next')) {
-    let results = await page.$$('lister-item-content');
-    scrape(stream, results);
+    const results = await page.evaluate(() => {
+      let data = [];
+      let elements = document.querySelectorAll('.lister-item-content');
+      elements.forEach((element) => {
+        let movie = {
+          title: element.childNodes[1].innerText,
+          description: element.childNodes[7].innerText,
+          director: element.childNodes[9].children[0].text,
+        };
+        data.push(JSON.stringify(movie));
+      });
+      return data;
+    });
     try {
-      await Promise.all([
-        page.waitForNavigation(),
-        page.click('.lister-page-next'),
-      ]);
+      scrape(stream, results);
+      if (page.$('.lister-page-next')) {
+        await Promise.all([
+          page.waitForNavigation(),
+          page.click('.lister-page-next'),
+        ]);
+      }
     } catch (error) {
       console.log(error);
+      browser.close();
+      break;
     }
   }
-
-  browser.close();
   stream.end();
-  stream.on('finish', () => {
-    console.error('Scrape Complete');
-  });
 };
 
 navigate();
