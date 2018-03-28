17627abd0bf83eda1a2b65088b122387e8be25e1
Bryce Sorensen <26312790+brycessorensen@users.noreply.github.com>
2018-03-27 23:54:56 -0700

    Add client-side search feature

--- server/routes.js
+++ server/routes.js
@@ -1,17 +1,28 @@
 const router = require('express').Router();
-const { initializeScrape } = require('../puppeteer.js');
+const { initializeScrape } = require('../web-crawler/web-crawler');
+const { search } = require('../database/query');
 
 router.get('/', (req, res) => {
   res.sendStatus(201);
 });
 
-router.get('/puppeteer', async (req, res) => {
+router.post('/web-crawler', async (req, res) => {
   try {
-    console.log(req.body);
-    initializeScrape(req.body);
-    res.sendStatus(201);
+    let { params } = req.body;
+    await initializeScrape(params);
+    res.send({ message: `${params.fileName} now searchable!` });
   } catch (error) {
-    res.send(JSON.stringify(error));
+    res.send(error);
+  }
+});
+
+router.get('/search', async (req, res) => {
+  let { query: { query } } = req;
+  try {
+    let results = await search(query);
+    res.send(results);
+  } catch (error) {
+    res.send(error);
   }
 });
 
