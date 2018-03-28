6d905d6cf3614f520034d844ba8ff918ab2c3a1b
Bryce Sorensen <26312790+brycessorensen@users.noreply.github.com>
2018-03-27 22:44:38 -0700

    Add dynamic search function for ElasticSearch

--- database/query.js
+++ database/query.js
@@ -1,43 +1,17 @@
 const { client } = require('./config');
 
 const search = async (queryString) => {
-  try {
-    let responseBody = await client.search({
-      q: queryString,
-      size: 15,
-    });
-    let { hits: { hits } } = responseBody;
-    let results = hits.map(hit => hit._source.title);
-    return results;
-  } catch (error) {
-    console.trace(error.message);
-  }
+  let responseBody = await client.search({
+    q: queryString,
+    size: 15,
+  });
+
+  let { hits: { hits } } = responseBody;
+  let results = hits.map(hit => hit._source.title);
+
+  return results;
 };
 
 module.exports = {
   search,
 };
-
-/*
-
-search('spielberg');
-
-Outputs:
-
-[ 'Catch Me If You Can',
-  'Minority Report',
-  'Schindler\'s List',
-  'E.T. the Extra-Terrestrial',
-  'Saving Private Ryan',
-  'Jaws',
-  'The Color Purple',
-  'Raiders of the Lost Ark',
-  'Jurassic Park',
-  'Close Encounters of the Third Kind',
-  'Bridge of Spies',
-  'Indiana Jones and the Last Crusade',
-  'Empire of the Sun',
-  'Indiana Jones and the Temple of Doom',
-  'Munich' ]
-
-*/
