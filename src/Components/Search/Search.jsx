a86f8e5c7ba52757d937db9b85ab70e042092c1b
Bryce Sorensen <26312790+brycessorensen@users.noreply.github.com>
2018-03-28 11:28:19 -0700

    Functional Search

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Search.css';
+import axios from 'axios';
 
 export default class Search extends Component {
   constructor(props) {
@@ -18,9 +19,7 @@ export default class Search extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    console.log('I should scrape the information!', this.state.value);
-
-
+    this.props.handleSearch(this.state.value);
     this.setState({ value: '' });
   }
 
