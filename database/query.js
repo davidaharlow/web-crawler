1f6eaff25a9697605905eb401c859845ef994ba3
Bryce Sorensen <26312790+brycessorensen@users.noreply.github.com>
2018-04-01 21:07:04 -0700

    Fix variable already declared conflict

--- database/query.js
+++ database/query.js
@@ -7,7 +7,6 @@ const search = async (queryString) => {
   });
 
   let { hits: { hits } } = responseBody;
-  let results = hits.map(hit => hit._source.title);
 
   let results = hits.map((hit) => {
     let result = Object.assign({}, hit._source);
